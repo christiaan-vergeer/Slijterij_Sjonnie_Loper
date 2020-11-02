6f992b50fba1c5dbc08192029502562248eb38e7
Luna Herder <luna@herderfactory.nl>
2020-11-02 15:49:46 +0100

    changes in Reservation

--- Slijterij_Sjonnie_Loper/Pages/Whiskey/Create.cshtml.cs
+++ Slijterij_Sjonnie_Loper/Pages/Whiskey/Create.cshtml.cs
@@ -9,6 +9,8 @@ namespace Slijterij_Sjonnie_Loper.Pages.Whiskey
 {
     public class CreateModel : PageModel
     {
+        public Core.Whiskey Whiskey { get; set; }
+
         public void OnGet()
         {
         }
--- Slijterij_Sjonnie_Loper/Pages/Whiskey/Reservation.cshtml.cs
+++ Slijterij_Sjonnie_Loper/Pages/Whiskey/Reservation.cshtml.cs
@@ -4,13 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Slijterij_Sjonnie_Loper.Data;
 
 namespace Slijterij_Sjonnie_Loper.Pages.Whiskey
 {
     public class ReservationModel : PageModel
     {
+        public Core.Whiskey Whiskey { get; set; }
+
         public void OnGet()
         {
+
         }
     }
 }
